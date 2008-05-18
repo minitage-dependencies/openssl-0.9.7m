32b0fb7ea1f6c2230738fa96fd80ba2ab0027e9a
kiorky <devnull@localhost>
2008-05-18 23:19:55 +0200

    0.4 changes pass 1

--- hooks/setenv.py
+++ hooks/setenv.py
@@ -1,16 +1,7 @@
 import os
 from minitage.core.common import append_env_var
 
-
-os_ldflags=''
-uname=os.uname()[0]
-if uname == 'Darwin':
-    os_ldflags=' -mmacosx-version-min=10.5.0'
-
 def patchopenssl(options,buildout):
-    append_env_var('LDFLAGS', [' %s ' % os_ldflags,buildout['flags']['ldflags']],sep=' ',before=False)
-    append_env_var('CFLAGS',  [buildout['flags']['cflags']],sep=' ',before=False)
-    append_env_var('LD_RUN_PATH',  [buildout['flags']['ldrun']],sep=':',before=False)
 
     # if gmake is setted. taking it as the make cmd !
     # be careful to have a 'gmake' in your path
